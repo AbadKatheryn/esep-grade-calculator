b0711760e5d512adfa031a1becd8ddf24424cb51
AbadKatheryn <katherynabad@ufl.edu>
2025-10-02 15:39:13 -0400

    Part 4: Added tests for Remaining Grades, Boundary Grades, and String Assignments

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,41 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+//Added test for C -KA
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 75, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 77, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test for D -KA
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 65, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 66, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -48,5 +83,106 @@ func TestGetGradeF(t *testing.T) {
 
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test for boundary of A -KA
+func TestBoundaryA(t *testing.T){
+	expected_value := "A"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 90, Assignment)
+	gradeCalculator.AddGrade("exam 1", 90, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 90, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test for boundary of B -KA
+func TestBoundaryB(t *testing.T){
+	expected_value := "B"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 80, Assignment)
+	gradeCalculator.AddGrade("exam 1", 80, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 80, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test for boundary of C -KA
+func TestBoundaryC(t *testing.T){
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 70, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 70, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test for boundary of D -KA
+func TestBoundaryD(t *testing.T){
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 60, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 60, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test to check assignment is a string -KA
+func TestStringAssignment(t *testing.T) {
+	expected_value := "assignment"
+
+	actual_value := Assignment.String()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected Assignment.String() to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test to check exam is a string -KA
+func TestStringExam(t *testing.T) {
+	expected_value := "exam"
+
+	actual_value := Exam.String()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected Exam.String() to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+//Added test to check essay is a string -KA
+func TestStringEssay(t *testing.T) {
+	expected_value := "essay"
+
+	actual_value := Essay.String()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected Essay.String() to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
